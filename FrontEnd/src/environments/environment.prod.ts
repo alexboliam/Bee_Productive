b9e6c48ef84fe8dc17f60684baafab78f0332029
ojdmytruk <55454373+ojdmytruk@users.noreply.github.com>
2019-12-17 14:01:38 +0200

    Add files via upload

---
+++ FrontEnd/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC91pH9NAPm-h6Tl1u2TjWDQ6LUEJ63bcg",
+    authDomain: "todoapp-87c50.firebaseapp.com",
+    databaseURL: "https://todoapp-87c50.firebaseio.com",
+    projectId: "todoapp-87c50",
+    storageBucket: "todoapp-87c50.appspot.com",
+    messagingSenderId: "952936604876"
+  }
+};
